11fd00ed017db5e919ac19a42715577075e047cd
good_chemist <iolgasergeevna@yandex.ru>
2023-11-17 17:12:39 +0800

    fix bug in test_arr.py

--- tests/test_arr.py
+++ tests/test_arr.py
@@ -1,8 +1,9 @@
+import pytest
 from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
